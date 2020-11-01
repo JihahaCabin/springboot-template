7754761854b797747935ef2b9710b06a42418f6c
zhengzhendu <zdzhengzhendu@163.com>
2020-11-01 14:28:01 +0800

    init

---
+++ src/main/java/com/example/springboottemplate/SpringbootTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboottemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootTemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootTemplateApplication.class, args);
+    }
+
+}
